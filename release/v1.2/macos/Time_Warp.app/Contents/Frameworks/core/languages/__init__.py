6732d1bac0c5d2527735f3dcdd4983c33a398e92
HB <james@amiga-fan.net>
2025-10-11 18:03:54 -0500

    stuff added and committed during rebase:

---
+++ release/v1.2/macos/Time_Warp.app/Contents/Frameworks/core/languages/__init__.py
@@ -0,0 +1,28 @@
+"""
+Time_Warp Language Support Modules
+=================================
+
+Contains implementations for supported programming languages:
+- BASIC interpreter
+- PILOT language processor
+- Logo turtle graphics engine
+- Perl script executor
+- Python script executor
+- JavaScript script executor
+"""
+
+from .pilot import PilotExecutor
+from .basic import BasicExecutor
+from .logo import LogoExecutor
+from .perl import PerlExecutor
+from .python_executor import PythonExecutor
+from .javascript_executor import JavaScriptExecutor
+
+__all__ = [
+    "PilotExecutor",
+    "BasicExecutor",
+    "LogoExecutor",
+    "PerlExecutor",
+    "PythonExecutor",
+    "JavaScriptExecutor",
+]
